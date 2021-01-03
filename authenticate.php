79669d0af6bce8878b87f1ced5f2f5bab2eae413
papcharis <papcharis@gmail.com>
2021-01-03 20:55:34 +0200

    authenticate error fix

--- authenticate.php
+++ authenticate.php
@@ -12,7 +12,7 @@ if ( !isset($_POST['username'], $_POST['password']) ) {
 }
 
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT username,passwordFROM accounts WHERE username = ?')) {
+if ($stmt = $con->prepare('SELECT username,password FROM accounts WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
@@ -44,5 +44,5 @@ if ($stmt = $con->prepare('SELECT username,passwordFROM accounts WHERE username 
 	$stmt->close();
 }
 
-close_con(con);
+close_con($con);
 ?>
