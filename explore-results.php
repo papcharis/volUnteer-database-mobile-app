ad2a4b3127957fc3df41b0f21769f6ddc8f4b22f
papcharis <papcharis@gmail.com>
2021-01-04 21:45:17 +0200

    Added explore section

--- explore-results.php
+++ explore-results.php
@@ -9,59 +9,129 @@
   <title>volUnteer</title>
   <link rel="stylesheet" href="explore-style.css" />
   <link href="https://fonts.googleapis.com/css2?family=Baloo:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
 
 </head>
-</html>
+<body>
+  <div class="logo-image">
+    <img src="./images/logo.png" alt="" />
+  </div>
+  <div class="logo">
+    <a href="./volunteer.php">Vol<mark>U</mark>nteer</a>
+  </div>
 
-<?php
-include "db_connection.php";
-$conn = open_con();
+  <?php
+  include "db_connection.php";
+  $conn = open_con();
 
-$search = $_GET['search'];
+  $search = $_GET['search'];
 
-$search = htmlspecialchars($search);
+  $search = htmlspecialchars($search);
 
 
-$sql = "SELECT ActName, volunteersNeeded,City,Start_Date ,DATEDIFF(End_Date,Start_Date) AS Duration FROM voluntary_activity
-        WHERE (ActName LIKE '%".$search."%') OR (City LIKE '%".$search."%')
-        OR (Organizer_ID IN (SELECT organizer_profile.Organizer_ID FROM organizer_profile JOIN voluntary_activity
-        ON organizer_profile.organizer_ID = voluntary_activity.organizer_ID
-        WHERE Name LIKE '%".$search."%')) AND Start_Date > curdate();";
+  $sql = "SELECT ActName, volunteersNeeded,voluntary_activity.City,Start_Date ,End_Date,Name FROM voluntary_activity JOIN organizer_profile
+          ON voluntary_activity.Organizer_ID = organizer_profile.organizer_ID
+          WHERE (ActName LIKE '%".$search."%') OR (voluntary_activity.City LIKE '%".$search."%')
+          OR (Name LIKE '%".$search."%') AND Start_Date > curdate()
+          ORDER BY Start_Date;";
 
-$result = $conn->query($sql);
+  $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    $RecPos = 280;
-    $ActNamePos = 284;
-    $DurationPos= 310;
-    $RolePos = 330;
-    $LocPos = 353;
-    $ButtonPos = 340;
-    $TextPos = 345;
+  $bg = array('#FFC59D','#D9B8CB','#C1D5D5','#FFC59D','#D9B8CB','#C1D5D5','#FFC59D','#D9B8CB','#C1D5D5');
+  $i = 0;
 
-    $text1 = 284;
-    $text2 = 310;
-    $text3 = 332;
-    $text4 = 353;
-    $text5 = 284;
-    $text6 = 284;
+  if ($result->num_rows > 0) {
+    // output data of each row
+    $RecPos = 211;
+    $DurationPos= 241;
+    $OrgPos = 261;
+    $LocPos = 284;
 
-    while($row = $result->fetch_assoc()){
+    $text1 = 215;
+    $text2 = 241;
+    $text3 = 263;
+    $text4 = 284;
+
+
+    while($row = $result->fetch_assoc()) {
       echo '<div class="rectangle"  style = "
       position: absolute;
       width: 320px;
       height: 100px;
       left: 27px;
       top: '.$RecPos.'px;
-      background: #C1D5D5;
+      background: '.$bg[$i].';
       border-radius: 15px;"></div>';
-    }
+
+      echo '<div class="duration" style = "
+      top: '.$DurationPos.'px;
+      "
+      >
+          <img src="./images/duration-small.svg" alt="" />
+      </div>';
+
+      echo '<div class="organizer" style = "
+      top: '.$OrgPos.'px;
+      ">
+          <img src="./images/organizer-small.svg" alt="" />
+      </div>';
+
+      echo '<div class="location" style = "
+      top: '.$LocPos.'px;
+      ">
+          <img src="./images/location-small.svg" alt="" />
+      </div>';
+
+      echo '<div class="actname" style = "
+      top: '.$text1.'px;
+      ">
+      '.$row['ActName'].'
+      </div>';
+
+      echo '<div class="durationText" style = "
+      top: '.$text2.'px;
+      ">
+      '.$row['Start_Date'].' - '.$row['End_Date'].'
+      </div>';
+
+      echo '<div class="orgText" style = "
+      top: '.$text3.'px;
+      ">
+      '.$row['Name'].'
+      </div>';
+
+      echo '<div class="locationText" style = "
+      top: '.$text4.'px;
+      ">
+      '.$row['City'].'
+      </div>';
+
+      $RecPos = $RecPos +120;
+      $DurationPos= $DurationPos + 120;
+      $OrgPos = $OrgPos + 120;
+      $LocPos = $LocPos + 120;
+
+      $text1 = $text1 + 120;
+      $text2 = $text2 + 120;
+      $text3 = $text3 + 120;
+      $text4 = $text4 + 120;
+
+      $i++;
+      if($i==8){
+        $i=0;
+      }
   }
 }
-else{
-  echo "THERE ARE NO ACTIVITIES RELATED TO YOUR SEARCH";
-}
+  else{
+    echo "THERE ARE NO ACTIVITIES RELATED TO YOUR SEARCH";
+  }
+  ?>
 
-?>
+  <div class="search">
+    <img src="./images/search_icon.svg" alt="" />
+  </div>
+  <div class="line"></div>
+  <div class="results-text">Results for: '<?= $search?> '</div>
+</body>
+</html>
--- explore.php
+++ explore.php
@@ -22,36 +22,121 @@
     <a href="./volunteer.php">Vol<mark>U</mark>nteer</a>
   </div>
   <div class="myevents">
-    <img src="./images/myevents.svg" alt="" />
+    <img src="./images/search_icon.svg" alt="" />
   </div>
   <div class="explore-header">
     Explore
   </div>
-  <div class="line"></div>
+  <div class="line2"></div>
   <div class="upcoming">
     Upcoming Voluntary Activities
   </div>
-  <div class="rectangle"></div>
-  <div class="duration">
-    <img src="./images/duration-small.svg" alt="" />
-  </div>  <div class="organizer">
-    <img src="./images/organizer-small.svg" alt="" />
-  </div>  <div class="location">
-    <img src="./images/location-small.svg" alt="" />
-  </div>
-  <a href="./events-description.php">
-    <div class="more"></div>
-    <div class="more-1">
-      More
-    </div>
-    <div class="more-2">
-      <img src="./images/arrow-small.svg" alt="" />
-    </div>
-  </a>
+	<form action="explore-results.php" method="GET" class="search_rectangle">
+		<input type="text" name="search" />
+		<input type="submit" value="Search" />
+	</form>
+
+<?php
+  include("db_connection.php");
+  $conn = open_con();
+
+  $sql = "SELECT ActName, volunteersNeeded,voluntary_activity.City,Start_Date ,End_Date,Name FROM voluntary_activity JOIN organizer_profile
+          ON voluntary_activity.Organizer_ID = organizer_profile.organizer_ID WHERE Start_Date > curdate()
+          ORDER BY Start_Date";
+
+  $result = $conn->query($sql);
+
+  $bg = array('#FFC59D','#D9B8CB','#C1D5D5','#FFC59D','#D9B8CB','#C1D5D5','#FFC59D','#D9B8CB','#C1D5D5');
+  $i = 0;
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    $RecPos = 224;
+    $DurationPos= 254;
+    $OrgPos = 274;
+    $LocPos = 297;
+
+    $text1 = 228;
+    $text2 = 254;
+    $text3 = 276;
+    $text4 = 297;
 
 
+    while($row = $result->fetch_assoc()) {
+      echo '<div class="rectangle"  style = "
+      position: absolute;
+      width: 320px;
+      height: 100px;
+      left: 27px;
+      top: '.$RecPos.'px;
+      background: '.$bg[$i].';
+      border-radius: 15px;"></div>';
 
-  
+      echo '<div class="duration" style = "
+      top: '.$DurationPos.'px;
+      "
+      >
+          <img src="./images/duration-small.svg" alt="" />
+      </div>';
+
+      echo '<div class="organizer" style = "
+      top: '.$OrgPos.'px;
+      ">
+          <img src="./images/organizer-small.svg" alt="" />
+      </div>';
+
+      echo '<div class="location" style = "
+      top: '.$LocPos.'px;
+      ">
+          <img src="./images/location-small.svg" alt="" />
+      </div>';
+
+      echo '<div class="actname" style = "
+      top: '.$text1.'px;
+      ">
+      '.$row['ActName'].'
+      </div>';
+
+      echo '<div class="durationText" style = "
+      top: '.$text2.'px;
+      ">
+      '.$row['Start_Date'].' - '.$row['End_Date'].'
+      </div>';
+
+      echo '<div class="orgText" style = "
+      top: '.$text3.'px;
+      ">
+      '.$row['Name'].'
+      </div>';
+
+      echo '<div class="locationText" style = "
+      top: '.$text4.'px;
+      ">
+      '.$row['City'].'
+      </div>';
+
+      $RecPos = $RecPos +120;
+      $DurationPos= $DurationPos + 120;
+      $OrgPos = $OrgPos + 120;
+      $LocPos = $LocPos + 120;
+
+      $text1 = $text1 + 120;
+      $text2 = $text2 + 120;
+      $text3 = $text3 + 120;
+      $text4 = $text4 + 120;
+
+      $i++;
+      if($i==8){
+        $i=0;
+      }
+  }
+}
+  else{
+    echo "THERE ARE NO ACTIVITIES RELATED TO YOUR SEARCH";
+  }
+  ?>
+
+
 </body>
 
 </html>
