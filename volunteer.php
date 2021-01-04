208482684afca20a658f95483eb256eb317c7f40
papcharis <papcharis@gmail.com>
2021-01-05 00:33:42 +0200

    Added logout

--- volunteer.php
+++ volunteer.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>volUnteer</title>
-  <link rel="stylesheet" href="volunteer-style.css" />
+  <link rel="stylesheet" type="text/css" href="volunteer-style.css" />
   <link href="https://fonts.googleapis.com/css2?family=Baloo:wght@400;500;600;700;800&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
@@ -23,6 +23,11 @@
     }
   ?>
 
+  <a href="logout.php">
+    <div class="logout">
+      <img src="./images/logout-icon.svg" alt="" />
+    </div>
+  </a>
 
   <div class="logo-image">
     <img src="./images/logo.png" alt="" />
@@ -30,6 +35,7 @@
   <div class="logo">
     <a href="./volunteer.php">Vol<mark>U</mark>nteer</a>
   </div>
+
 
   <a href="eventschedule.php">
     <div class="rectangle-1"></div>
