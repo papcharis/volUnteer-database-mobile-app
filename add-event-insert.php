9f6c1ea54a97974a03a2eab39041620e00e54797
papcharis <papcharis@gmail.com>
2021-01-05 21:28:52 +0200

    add event fix

--- add-event-insert.php
+++ add-event-insert.php
@@ -9,13 +9,12 @@
   include 'db_connection.php';
   $conn = open_con();
 
-  $stmt = $conn->prepare('SELECT organizer_profile.organizer_ID
-                          FROM organizer_profile JOIN accountso on organizer_profile.organizer_ID = accountso.organizer_ID
-                          WHERE username = ?');
+  $stmt = $conn->prepare('SELECT organizer_ID FROM accountso WHERE username = ?');
 
   $stmt->bind_param('s',$_SESSION['username']);
   $stmt->execute();
   $stmt->store_result();
+
   if($stmt->num_rows>0){
     $stmt->bind_result($orgID);
     $stmt->fetch();
@@ -23,6 +22,7 @@
   else{
     echo "Query did not work out.";
   }
+
   $randomID  = str_pad(mt_rand(1,9999999),7,'0',STR_PAD_LEFT);
 
   if (!isset($randomID, $_POST['activity_name'],$_POST['city'],$_POST['address'],$_POST['zipcode'],$_POST['startdate'],$_POST['enddate'], $orgID)) {
@@ -30,7 +30,7 @@
   	exit('Data not received, try again');
   }
   if($stmt2 = $conn->prepare('INSERT INTO voluntary_activity (Activity_ID, ActName, City,Street,ZipCode,VolunteersNeeded,Start_Date,End_Date,Organizer_ID) VALUES (?, ?, ?, ?,?, ?, ?, ? , ?)')){
-    $stmt2->bind_param('sssssssss', $randomID, $_POST['activity_name'],$_POST['city'],$_POST['address'],$_POST['zipcode'],$_POST['startdate'],$_POST['enddate'], $orgID);
+    $stmt2->bind_param('sssssssss', $randomID, $_POST['activity_name'],$_POST['city'],$_POST['address'],$_POST['zipcode'], $_POST['numofvol'], $_POST['startdate'],$_POST['enddate'], $orgID);
     $stmt2->execute();
   }
   else{
