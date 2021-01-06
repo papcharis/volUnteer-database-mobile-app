fbbb485d8dc8976709d3a6dea228c033583f1d2d
papcharis <papcharis@gmail.com>
2021-01-07 00:26:37 +0200

    Added chosen and fixed handle-volunteers

--- handle-volunteers-chosen.php
+++ handle-volunteers-chosen.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>volUnteer</title>
-  <link rel="stylesheet" href="handle-volunteers-rejected-style.css" />
+  <link rel="stylesheet" href="handle-volunteers-chosen-style.css" />
   <link href="https://fonts.googleapis.com/css2?family=Baloo:wght@400;500;600;700;800&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
@@ -31,7 +31,7 @@
     include 'db_connection.php';
 
     $conn = open_con();
-    
+
     session_start();
     if (!isset($_SESSION['loggedin'])) {
         header('Location: login.html');
@@ -39,15 +39,15 @@
     }
 
     $act_id = $_GET["ActID"];
-    
+
     $sql0 = "SELECT ActName FROM voluntary_activity WHERE Activity_ID='$act_id'";
     $sql0_result = $conn->query($sql0);
     if ($sql0_result->num_rows > 0) {
         $record = $sql0_result->fetch_row();
         $act_name = $record[0];
-    } 
+    }
 
-    $act_name = strlen($act_name) > 25 ? substr($act_name,0,25)."..." : $act_name;
+    $act_name = strlen($act_name) > 23 ? substr($act_name,0,23)."..." : $act_name;
     echo '<div class="myevents-header">
           '.$act_name.'
           </div>';
@@ -57,13 +57,11 @@
     <a href="handle-volunteers.php?ActID='.urldecode($act_id).'">Applied </a>
     </div>';
     echo '<div class="chosen">
-    <a href="handle-volunteers-choosen.php?ActID='.urldecode($act_id).'">Chosen</a>
-    </div>';
-    echo '<div class="rejected">
-    <a href="handle-volunteers-rejected.php?ActID='.urldecode($act_id).'">Rejected</a>
+    <a href="handle-volunteers-chosen.php?ActID='.urldecode($act_id).'">Chosen</a>
     </div>';
 
-    $sql1 = "SELECT Volunteer_Username FROM applies WHERE Activity_ID='$act_id'";
+
+    $sql1 = "SELECT Volunteer_Username,Role_Title FROM hires WHERE Activity_ID='$act_id'";
     $result = $conn->query($sql1);
 
     $age=0;
@@ -76,7 +74,8 @@
       $TikPos= 295;
       $XPos = 295;
       $NamePos = 270;
-      $AgePos = 285;
+      $AgePos = 288;
+      $RolePos = 310;
 
 
 
@@ -93,7 +92,7 @@
         }
         else {
             echo "QUERY ERROR";
-        } 
+        }
 
         echo '<div class="rectangle" style="top: '.$RecPos.'px"></div>';
         if($sex == 'F') {
@@ -108,19 +107,10 @@
         }
 
         echo '<div class="Name" style="top:'.$NamePos.'px"> '.$name.' </div>';
-        echo '<div class="Age" style="top:'.$AgePos.'px">Age = '.$age.' </div>';
+        echo '<div class="Age" style="top:'.$AgePos.'px">Age : '.$age.' </div>';
+        echo '<div class="Role" style="top:'.$RolePos.'px">Role : '.$row['Role_Title'].' </div>';
 
-        echo '<div class="Tik"  style = 
-          "top: '.$TikPos.'px;
-           left: 250px;  ">
-              <img src="./images/tik.svg" alt="" />
-           </div>';
 
-           echo '<div class="Xi" style = 
-           "top: '.$XPos.'px;
-            left: 300px;  ">
-           <img src="./images/xi.svg" alt="" />
-          </div>';
 
         $RecPos = $RecPos + 120;
         $ImPos = $ImPos + 120;
@@ -128,17 +118,18 @@
         $XPos = $XPos + 120;
         $NamePos = $NamePos+120;
         $AgePos = $AgePos +120;
+        $RolePos = $RolePos + 120;
       }
 
 
     }
 
 
-  
 
 
 
-    
+
+
 
 
     $conn->close();
--- handle-volunteers.php
+++ handle-volunteers.php
@@ -31,7 +31,7 @@
     include 'db_connection.php';
 
     $conn = open_con();
-    
+
     session_start();
     if (!isset($_SESSION['loggedin'])) {
         header('Location: login.html');
@@ -39,13 +39,13 @@
     }
 
     $act_id = $_GET["ActID"];
-    
+
     $sql0 = "SELECT ActName FROM voluntary_activity WHERE Activity_ID='$act_id'";
     $sql0_result = $conn->query($sql0);
     if ($sql0_result->num_rows > 0) {
         $record = $sql0_result->fetch_row();
         $act_name = $record[0];
-    } 
+    }
 
     $act_name = strlen($act_name) > 23 ? substr($act_name,0,23)."..." : $act_name;
     echo '<div class="myevents-header">
@@ -57,11 +57,9 @@
     <a href="handle-volunteers.php?ActID='.urldecode($act_id).'">Applied </a>
     </div>';
     echo '<div class="chosen">
-    <a href="handle-volunteers-choosen.php?ActID='.urldecode($act_id).'">Chosen</a>
+    <a href="handle-volunteers-chosen.php?ActID='.urldecode($act_id).'">Chosen</a>
     </div>';
-    echo '<div class="rejected">
-    <a href="handle-volunteers-rejected.php?ActID='.urldecode($act_id).'">Rejected</a>
-    </div>';
+
 
     $sql1 = "SELECT Volunteer_Username FROM applies WHERE Activity_ID='$act_id' AND Volunteer_Username NOT IN (SELECT Volunteer_Username FROM hires WHERE Activity_ID='$act_id ')";
     $result = $conn->query($sql1);
@@ -76,7 +74,7 @@
       $TikPos= 295;
       $XPos = 295;
       $NamePos = 270;
-      $AgePos = 285;
+      $AgePos = 287;
 
 
 
@@ -93,7 +91,7 @@
         }
         else {
             echo "QUERY ERROR";
-        } 
+        }
 
         echo '<div class="rectangle" style="top: '.$RecPos.'px"></div>';
         if($sex == 'F') {
@@ -108,9 +106,9 @@
         }
 
         echo '<div class="Name" style="top:'.$NamePos.'px"> '.$name.' </div>';
-        echo '<div class="Age" style="top:'.$AgePos.'px">Age = '.$age.' </div>';
+        echo '<div class="Age" style="top:'.$AgePos.'px">Age : '.$age.' </div>';
 
-        echo '<div class="Tik"  style = 
+        echo '<div class="Tik"  style =
           "top: '.$TikPos.'px;
            left: 250px;  ">
               <a href="handle-volunteers-Tik.php?ActID='.urldecode($act_id).'&VolUserName='.$curr_username.'">
@@ -118,7 +116,7 @@
               </a>
            </div>';
 
-           echo '<div class="Xi" style = 
+           echo '<div class="Xi" style =
            "top: '.$XPos.'px;
             left: 300px;  ">
             <a <a href="handle-volunteers-Xi.php?ActID='.urldecode($act_id).'">
@@ -136,14 +134,6 @@
 
 
     }
-
-
-  
-
-
-
-    
-
 
     $conn->close();
   ?>
