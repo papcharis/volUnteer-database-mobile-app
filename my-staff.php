8418a03d39f2edf7b6db5b731ac7b3f674693ffc
charitink <haritinikoutra@gmail.com>
2021-01-08 16:15:26 +0200

    nostaff message fix

--- my-staff.php
+++ my-staff.php
@@ -82,7 +82,19 @@
     }
   }
   else{
-    echo "YOU HAVE NO STAFF RIGHT NOW";
+    echo '<div class="message" style= "position: absolute;
+              width: 272px;
+              height: 51px;
+              left: 51px;
+              top: 355px;
+              font-family: Montserrat;
+              font-style: normal;
+              font-weight: 500;
+              font-size: 24px;
+              line-height: 29px;
+              text-align: center;
+              color: #000000;">';
+    echo "You have not staff right now";
   }
  ?>
 
