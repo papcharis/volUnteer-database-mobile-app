06a73da828c45376e5d0643005e15e08416a59f4
papcharis <papcharis@gmail.com>
2021-01-08 19:46:23 +0200

    Added favicon

--- myevents.php
+++ myevents.php
@@ -10,6 +10,7 @@
   <link rel="stylesheet" href="myevents-style.css" />
   <link href="https://fonts.googleapis.com/css2?family=Baloo:wght@400;500;600;700;800&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <link rel="SHORTCUT ICON" HREF="./images/favicon.png">
 </head>
 
 <body>
